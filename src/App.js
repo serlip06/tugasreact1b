063c240bfd811c273c5f5c5b713972f758ae3cd8
serlip06 <114634943+serlip06@users.noreply.github.com>
2022-12-13 12:13:54 +0700

    tugasreact

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <code>selamat belajar reactjs</code> 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
